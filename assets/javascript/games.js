b7c8b8d9e18b3a9facaee5857846fdd0ee5c7706
lidia <lidiabara7@gmail.com>
2019-06-18 20:12:53 -0700

    added some style to the file and added the how to win or lose the game

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,15 +1,44 @@
-//The player will have to guess the answer, the player will guess with numbers. 
-//Here's how the app works:
-//There will be four crystals displayed as buttons on the page.
-//The player will be shown a random number at the start of the game.
-//When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-//Your game will hide this amount until the player clicks a crystal.
-//When they do click one, update the player's score counter.
-//The player wins if their total score matches the random number from the beginning of the game.
-//The player loses if their score goes above the random number.
-//The game restarts whenever the player wins or loses.
-//When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-//The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
-//The random number shown at the start of the game should be between 19 - 120.
-//Each crystal should have a random hidden value between 1 - 12.
+        var wins = 0
+        var losses = 0
+        var playerScore;
+        var imgArr = [];
+
+        //need to generate random number between 19 and 120
+        var randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        console.log(randomNumber)
+        //insert that random number into the html file
+        $("#randomNumber").text(randomNumber);
+
+
+        for (i = 0; i < 4; i++) {
+            imgArr.push(Math.floor(Math.random() * 12 + 1));
+            console.log(imgArr[i])
+        }
+        var imgbtn = $("#totalScore");
+        imgbtn.attr("data-crystals", imgArr[i]);
+        $("#totalScore").append(imgArr);
+        console.log(imgbtn)
+
+        $("#shapeImage").on("click", function () {
+            var imageValue = $("<img>");
+            $("#totalScore").text(attr("data-crystals"));
+            playerScore += imageValue;
+
+        });
+
+
+        //wins +1 if player score equals to the random number
+        //computer generates another random number and the value of the images changes
+        if (playerScore === randomNumber) {
+            wins++
+            var randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+            var imageValue = Math.floor(Math.random() * 12);
+        }
+        // if the player score is higher than the random number losses +1
+        // computer generates another random number and the value of the images changes
+        if (playerScore > randomNumber) {
+            losses++
+            var randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+            var imageValue = Math.floor(Math.random() * 12);
+        }
 
